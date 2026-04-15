cc1e4219a30c5f5986c6d9468a4d9e135d6c38d6
Dan Green <dgreenj@gmail.com>
2026-04-15 20:03:11 +0000

    Remove anjins (Japan/Korea) legacy files, rename .Rproj

--- custom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Check if the current page is analyst_dash.html
-  if (window.location.pathname.includes("analyst_dash")) {
-    // Hide the footer
-    document.querySelector(".page-footer").style.display = "none";
-  }
-});
--- r_scipts/remove_problem_files.R
+++
@@ -1,8 +0,0 @@
-# Delete the problematic .Rmd files
-file.remove(list.files("daily_itineraries", pattern = "\\.Rmd$", full.names = TRUE))
-
-# Delete the incorrectly named notes files
-file.remove(list.files("notes", pattern = "notes_10_10_|notes_11_11_", full.names = TRUE))
-
-# Re-run the generator with the corrected script
-result <- main_complete_generator()
